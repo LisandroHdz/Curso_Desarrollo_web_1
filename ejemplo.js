31765a3691b33fb61567f8da3ec2b13610313921
LisandroHdz <99113543+LisandroHdz@users.noreply.github.com>
2022-12-18 13:40:14 -0600

    Practicas en js
    Practicas en js

---
+++ ejemplo.js
@@ -0,0 +1,35 @@
+//string
+var saludos = ("hola mundo");
+//number
+let numero = 25;
+//boolean 
+//let verdadero = true;
+//let falso = false;
+//undefined let undefined =   "undefined";
+//nulo let nulo = null;
+
+console.log(saludos);
+console.log(numero);
+let nombre =("Lisandro");
+console.log(`Hola me llamo ${nombre} y tengo ${numero}`);
+
+let numero1 = 60;
+let numero2 = 40;
+
+var suma = (numero1 + numero2);
+console.log (suma);
+
+let x = 5;
+let y = 6;
+let o = 4;
+let a = 2;
+let b = 9;
+
+console.log (x = a);
+console.log (x +=a);
+console.log (x *= a);
+
+console.log (o == b);
+console.log (y != o);
+
+

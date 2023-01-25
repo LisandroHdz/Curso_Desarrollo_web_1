a179fbe5b26e58a5b0314e4604ef8fc4373d18b4
LisandroHdz <99113543+LisandroHdz@users.noreply.github.com>
2023-01-25 02:19:08 -0600

    Tarea 7
    Tarea 7

---
+++ semana_siete/funciones.js
@@ -0,0 +1,36 @@
+//let edad = 15;
+
+// if (edad >= 18){
+//    console.log("Valido")
+// }else{
+//    console.log("No valido")
+//}
+
+/*const futbol = [
+    {id: 2, nombre: "Pumas", estadio: "CU", ubicación: "CDMX", Fundación: 1954},
+    {id: 3, nombre: "Tigres", estadio: "Universitario", ubicación: "MTY", Fundación: 1960},
+    {id: 4, nombre: "Chivas", estadio: "Akron", ubicación: "GDL", Fundación: 1906},
+    {id: 5, nombre: "Cruz Azul", estadio: "Azteca", ubicación: "CDMX", Fundación: 1927},
+];
+
+for (let i = 0; i < futbol.length; i++){
+    console.log("Array completo: ", futbol[i]);
+}
+
+
+let edad = 40
+
+switch(edad){
+    case 18:
+        console.log("Te regalamos 2x1 en helados");
+        break;
+    case 25:
+        console.log("Llevate un combo amigos por tan solo 150pesos");
+        break;
+    case 40:
+        console.log("Compra el siguiente boleto al 50%");
+        break;
+    default:
+        console.log("Bienvenido a Cineclub!!");
+}
+*/

3dcf14be19d7a4113933b88f4fa62c534beb9648
LisandroHdz <99113543+LisandroHdz@users.noreply.github.com>
2023-01-31 01:26:12 -0600

    Tarea

---
+++ semana_ocho/funciones.js
@@ -0,0 +1,37 @@
+let zapatos = [
+    {marca: 'Nike', stock: 8, precios: 350.3},
+    {marca: 'Puma', stock: 7, precios: 80.5},
+    {marca: 'Adidas', stock: 6, precios: 370.41},
+    {marca: 'Converse', stock: 8, precios: 120.8}
+]
+
+/*zapatos.forEach(prueba =>{
+    console.log(prueba.marca);
+    console.log(prueba.stock);
+})*/
+
+/*let mayor = zapatos.filter(elemento => elemento.precios > 200)
+console.log(mayor);
+
+zapatos.sort((a,b)=>{
+    if(a.marca == b.marca){
+        return 0;
+    }
+    if(b.marca < a.marca){
+        return -1;
+    }
+    return 1;
+});
+
+console.log(zapatos);
+let preciomax = 0;
+
+zapatos.forEach(elemnto =>{
+    console.log(elemnto.precios);
+    preciomax = Math.max(elemnto.precios);
+})
+
+console.log('El precio maximo es: ', preciomax);*/
+
+
+
